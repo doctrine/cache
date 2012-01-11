0ce31fab9ec4b9d96fb6ec86654987956b62de52
Hugo Hamon <hugo.hamon@sensio.com>
2012-01-11 21:31:49 +0100

    [Cache] removed unused local variable $idCacheKey in CacheProvider::getNamespacedId() method.

--- CacheProvider.php
+++ CacheProvider.php
@@ -133,7 +133,6 @@ abstract class CacheProvider implements Cache
     {
         $namespaceCacheKey = sprintf(self::DOCTRINE_NAMESPACE_CACHEKEY, $this->namespace);
         $namespaceVersion  = ($this->doContains($namespaceCacheKey)) ? $this->doFetch($namespaceCacheKey) : 1;
-        $idCacheKey        = strtr($id,  array('[' => '', ']' => ''));
 
         return sprintf('%s[%s][%s]', $this->namespace, $id, $namespaceVersion);
     }
