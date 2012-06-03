ef105da904b5f1c3043b3f88e3b7d08b108df5a1
H. Westphal <westphal@secure-net-concepts.de>
2012-06-03 12:50:35 +0200

    Fixed Cache docblocks (closes #148)

--- Cache.php
+++ Cache.php
@@ -45,7 +45,7 @@ interface Cache
      * Fetches an entry from the cache.
      *
      * @param string $id cache id The id of the cache entry to fetch.
-     * @return string The cached data or FALSE, if no cache entry exists for the given id.
+     * @return mixed The cached data or FALSE, if no cache entry exists for the given id.
      */
     function fetch($id);
 
@@ -61,7 +61,7 @@ interface Cache
      * Puts data into the cache.
      *
      * @param string $id The cache id.
-     * @param string $data The cache entry/data.
+     * @param mixed $data The cache entry/data.
      * @param int $lifeTime The lifetime. If != 0, sets a specific lifetime for this cache entry (0 => infinite lifeTime).
      * @return boolean TRUE if the entry was successfully stored in the cache, FALSE otherwise.
      */
