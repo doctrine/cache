450bceb55f54e08eb6786ff11bb93c5e207c111d
Fabio B. Silva <fabio.bat.silva@gmail.com>
2012-06-12 18:08:58 -0300

    test filetime

--- FilesystemCacheTest.php
+++ FilesystemCacheTest.php
@@ -26,13 +26,45 @@ class FilesystemCacheTest extends CacheTest
         return $this->driver;
     }
 
+    public function testLifetime()
+    {
+        $cache = $this->_getCacheDriver();
+
+        // Test save
+        $cache->save('test_key', 'testing this out', 10);
+
+        // Test contains to test that save() worked
+        $this->assertTrue($cache->contains('test_key'));
+
+        // Test fetch
+        $this->assertEquals('testing this out', $cache->fetch('test_key'));
+
+        // access private methods
+        $getFilename        = new \ReflectionMethod($cache, 'getFilename');
+        $getNamespacedId    = new \ReflectionMethod($cache, 'getNamespacedId');
+
+        $getFilename->setAccessible(true);
+        $getNamespacedId->setAccessible(true);
+
+        $id     = $getNamespacedId->invoke($cache, 'test_key');
+        $path   = $getFilename->invoke($cache, $id);
+        $value  = include $path;
+
+        // update lifetime
+        $value['lifetime'] = $value['lifetime'] - 20;
+        file_put_contents($path, '<?php return unserialize(' . var_export(serialize($value), true) . ');');
+
+        // test expired data
+        $this->assertFalse($cache->contains('test_key'));
+        $this->assertFalse($cache->fetch('test_key'));
+    }
+
     public function testGetStats()
     {
         $cache = $this->_getCacheDriver();
         $stats = $cache->getStats();
         $this->assertNull($stats);
     }
-
 
     public function tearDown()
     {
