fb5ffe96a8665dfe01f83caa01f500ece88df211
fabios <fabios@nationalfibre.net>
2013-09-26 15:17:52 -0400

    Temporary apcu fix

--- lib/Doctrine/Common/Cache/ApcCache.php
+++ lib/Doctrine/Common/Cache/ApcCache.php
@@ -80,6 +80,13 @@ class ApcCache extends CacheProvider
         $info = apc_cache_info();
         $sma  = apc_sma_info();
 
+        // @TODO - Temporary fix @see https://github.com/krakjoe/apcu/pull/42
+        if (PHP_VERSION_ID >= 50500) {
+            $info['num_hits']   = isset($info['num_hits'])   ? $info['num_hits']   : $info['nhits'];
+            $info['num_misses'] = isset($info['num_misses']) ? $info['num_misses'] : $info['nmisses'];
+            $info['start_time'] = isset($info['start_time']) ? $info['start_time'] : $info['stime'];
+        }
+
         return array(
             Cache::STATS_HITS             => $info['num_hits'],
             Cache::STATS_MISSES           => $info['num_misses'],
