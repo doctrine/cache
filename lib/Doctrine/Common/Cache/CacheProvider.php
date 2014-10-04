f7b72fa98f75a26b88fb391739c2fd95c4578afc
Adirelle <adirelle@gmail.com>
2014-10-04 14:23:03 +0200

    Added interfaces for the various capabilities of CacheProvider.

--- lib/Doctrine/Common/Cache/CacheProvider.php
+++ lib/Doctrine/Common/Cache/CacheProvider.php
@@ -29,7 +29,7 @@ namespace Doctrine\Common\Cache;
  * @author Roman Borschel <roman@code-factory.org>
  * @author Fabio B. Silva <fabio.bat.silva@gmail.com>
  */
-abstract class CacheProvider implements Cache, MultiGetCache
+abstract class CacheProvider implements Cache, FlushableCache, NamespacedCache, ClearableCache, MultiGetCache
 {
     const DOCTRINE_NAMESPACE_CACHEKEY = 'DoctrineNamespaceCacheKey[%s]';
 
