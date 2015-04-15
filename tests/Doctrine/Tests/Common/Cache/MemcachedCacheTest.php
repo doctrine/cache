c9eadeb743ac6199f7eec423cb9426bc518b7b03
Marco Pivetta <ocramius@gmail.com>
2015-04-15 01:11:59 +0100

    Disabling testing `false` against `Memcached`, since it is unsupported

--- tests/Doctrine/Tests/Common/Cache/MemcachedCacheTest.php
+++ tests/Doctrine/Tests/Common/Cache/MemcachedCacheTest.php
@@ -53,4 +53,18 @@ class MemcachedCacheTest extends CacheTest
         $driver->setMemcached($this->memcached);
         return $driver;
     }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @dataProvider falseCastedValuesProvider
+     */
+    public function testFalseCastedValues($value)
+    {
+        if (false === $value) {
+            $this->markTestIncomplete('Memcached currently doesn\'t support saving `false` values. ');
+        }
+
+        parent::testFalseCastedValues($value);
+    }
 }
