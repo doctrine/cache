b3cdfc6caa3fdabedea2c0b6c10547e9a3235ed2
Tobias Schultze <webmaster@tubo-world.de>
2015-10-08 23:11:43 +0200

    fix redis cache deleting non-existing entries

--- lib/Doctrine/Common/Cache/RedisCache.php
+++ lib/Doctrine/Common/Cache/RedisCache.php
@@ -99,7 +99,7 @@ class RedisCache extends CacheProvider
      */
     protected function doDelete($id)
     {
-        return $this->redis->delete($id) > 0;
+        return $this->redis->delete($id) >= 0;
     }
 
     /**
