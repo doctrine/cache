b36c68a0edbb46f5375e1a20dff711ce6ba5160c
Tobias Schultze <webmaster@tubo-world.de>
2015-11-17 18:29:50 +0100

    fix fetchMultiple when cached data is false

--- tests/Doctrine/Tests/Common/Cache/PhpFileCacheTest.php
+++ tests/Doctrine/Tests/Common/Cache/PhpFileCacheTest.php
@@ -10,55 +10,14 @@ use Doctrine\Common\Cache\PhpFileCache;
  */
 class PhpFileCacheTest extends BaseFileCacheTest
 {
-    /**
-     * {@inheritDoc}
-     *
-     * @dataProvider provideDataToCache
-     */
-    public function testSetContainsFetchDelete($value)
+    public function provideDataToCache()
     {
-        if (is_object($value) && ! method_exists($value, '__set_state')) {
-            $this->markTestSkipped('PhpFileCache only allows objects that implement __set_state() and fully support var_export()');
-        }
+        $data = parent::provideDataToCache();
 
-        if (0.0 === $value) {
-            $cache = $this->_getCacheDriver();
+        unset($data['object']); // PhpFileCache only allows objects that implement __set_state() and fully support var_export()
+        unset($data['float_zero']); // var_export exports float(0) as int(0)
 
-            $this->assertTrue($cache->save('key', $value));
-            $this->assertTrue($cache->contains('key'));
-            $this->assertSame(0, $cache->fetch('key'), 'var_export exports float(0) as int(0) so we assert against 0 as integer');
-
-            $this->assertTrue($cache->delete('key'));
-            $this->assertFalse($cache->contains('key'));
-            $this->assertFalse($cache->fetch('key'));
-        } else {
-            parent::testSetContainsFetchDelete($value);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @dataProvider provideDataToCache
-     */
-    public function testUpdateExistingEntry($value)
-    {
-        if (is_object($value) && ! method_exists($value, '__set_state')) {
-            $this->markTestSkipped('PhpFileCache only allows objects that implement __set_state() and fully support var_export()');
-        }
-
-        if (0.0 === $value) {
-            $cache = $this->_getCacheDriver();
-
-            $this->assertTrue($cache->save('key', 'old-value'));
-            $this->assertTrue($cache->contains('key'));
-
-            $this->assertTrue($cache->save('key', $value));
-            $this->assertTrue($cache->contains('key'));
-            $this->assertSame(0, $cache->fetch('key'), 'var_export exports float(0) as int(0) so we assert against 0 as integer');
-        } else {
-            parent::testUpdateExistingEntry($value);
-        }
+        return $data;
     }
 
     public function testLifetime()
