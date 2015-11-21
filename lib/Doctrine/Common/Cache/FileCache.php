e9e3f7b99b993feddeea011916132965bf26b87f
Tobias Schultze <webmaster@tubo-world.de>
2015-11-22 00:16:58 +0100

    fix file cache naming under windows surpassing MAX_PATH length and thus failing

--- lib/Doctrine/Common/Cache/FileCache.php
+++ lib/Doctrine/Common/Cache/FileCache.php
@@ -115,10 +115,12 @@ abstract class FileCache extends CacheProvider
         $hash = hash('sha256', $id);
 
         // This ensures that the filename is unique and that there are no invalid chars in it.
-        if ('' === $id || strlen($id) > ((255 - strlen($this->extension)) / 2)) {
-            // Most filesystems have a limit of 255 chars for each path component. So if the id in hex representation
-            // plus the extension would surpass the limit, we use the hash instead. The prefix prevents collisions
-            // between the hash and bin2hex.
+        if ('' === $id || strlen($id) * 2 + strlen($this->extension) > 255 ||
+            (defined('PHP_WINDOWS_VERSION_BUILD') && strlen($this->directory) + 4 + strlen($id) * 2 + strlen($this->extension) > 259)) {
+            // Most filesystems have a limit of 255 chars for each path component. On Windows the the whole path is limited
+            // to 260 chars (including terminating null char). Using long UNC ("\\?\" prefix) does not work with the PHP API.
+            // So if the id in hex representation would surpass the limit, we use the hash instead. The prefix prevents
+            // collisions between the hash and bin2hex.
             $filename = '_' . $hash;
         } else {
             $filename = bin2hex($id);
