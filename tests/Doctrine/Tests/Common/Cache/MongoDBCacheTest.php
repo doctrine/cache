93d11352b0d8d4da3169a9b86983f31658735db7
Marco Pivetta <ocramius@gmail.com>
2015-12-09 17:50:58 +0100

    `MongoCollection` expects constructor parameters - we don't have them

--- tests/Doctrine/Tests/Common/Cache/MongoDBCacheTest.php
+++ tests/Doctrine/Tests/Common/Cache/MongoDBCacheTest.php
@@ -52,7 +52,7 @@ class MongoDBCacheTest extends CacheTest
     public function testMongoCursorExceptionsDoNotBubbleUp()
     {
         /* @var $collection \MongoCollection|\PHPUnit_Framework_MockObject_MockObject */
-        $collection = $this->getMock('MongoCollection');
+        $collection = $this->getMock('MongoCollection', array(), array(), '', false);
 
         $collection->expects(self::once())->method('update')->willThrowException(new \MongoCursorException());
 
