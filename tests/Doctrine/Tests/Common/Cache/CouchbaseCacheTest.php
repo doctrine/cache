aa018c25d31458f16175255d79e86d33a47af0f4
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-08 00:20:07 +0100

    Typo, changed "Exception" to "\Exception"

--- tests/Doctrine/Tests/Common/Cache/CouchbaseCacheTest.php
+++ tests/Doctrine/Tests/Common/Cache/CouchbaseCacheTest.php
@@ -16,7 +16,7 @@ class CouchbaseCacheTest extends CacheTest
     {
         try {
             $this->couchbase = new Couchbase('127.0.0.1', 'Administrator', 'password', 'default');
-        } catch(Exception $ex) {
+        } catch(\Exception $ex) {
              $this->markTestSkipped('Could not instantiate the Couchbase cache because of: ' . $ex);
         }
     }
