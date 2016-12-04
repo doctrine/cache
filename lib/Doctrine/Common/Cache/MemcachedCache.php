cda9f915bc88e69f8588760d068a9bd77682ec0e
Jonas HAOUZI <jonas@viscaweb.com>
2016-12-04 14:39:32 +0100

    Stop checking the value returned by Memcached (but rely only on the status code)

--- lib/Doctrine/Common/Cache/MemcachedCache.php
+++ lib/Doctrine/Common/Cache/MemcachedCache.php
@@ -82,8 +82,8 @@ class MemcachedCache extends CacheProvider
      */
     protected function doContains($id)
     {
-        return false !== $this->memcached->get($id)
-            && $this->memcached->getResultCode() === Memcached::RES_SUCCESS;
+        $this->memcached->get($id);
+        return $this->memcached->getResultCode() === Memcached::RES_SUCCESS;
     }
 
     /**
