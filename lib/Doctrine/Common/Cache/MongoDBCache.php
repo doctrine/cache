665ad011e14a92756637fb2da55b79737c72185e
Andreas Braun <git@alcaeus.org>
2017-05-26 14:01:57 +0200

    Add cache provider for ext-mongodb

--- lib/Doctrine/Common/Cache/MongoDBCache.php
+++ lib/Doctrine/Common/Cache/MongoDBCache.php
@@ -19,10 +19,8 @@
 
 namespace Doctrine\Common\Cache;
 
-use MongoBinData;
 use MongoCollection;
-use MongoCursorException;
-use MongoDate;
+use MongoDB\Collection;
 
 /**
  * MongoDB cache provider.
@@ -55,31 +53,33 @@ class MongoDBCache extends CacheProvider
     const EXPIRATION_FIELD = 'e';
 
     /**
-     * @var MongoCollection
+     * @var CacheProvider
      */
-    private $collection;
-
-    /**
-     * @var bool
-     */
-    private $expirationIndexCreated = false;
+    private $provider;
 
     /**
      * Constructor.
      *
      * This provider will default to the write concern and read preference
-     * options set on the MongoCollection instance (or inherited from MongoDB or
+     * options set on the collection instance (or inherited from MongoDB or
      * MongoClient). Using an unacknowledged write concern (< 1) may make the
      * return values of delete() and save() unreliable. Reading from secondaries
      * may make contain() and fetch() unreliable.
      *
      * @see http://www.php.net/manual/en/mongo.readpreferences.php
      * @see http://www.php.net/manual/en/mongo.writeconcerns.php
-     * @param MongoCollection $collection
+     * @param MongoCollection|Collection $collection
      */
-    public function __construct(MongoCollection $collection)
+    public function __construct($collection)
     {
-        $this->collection = $collection;
+        if ($collection instanceof MongoCollection) {
+            @trigger_error('Using a MongoCollection instance for creating a cache adapter is deprecated and will be removed in 2.0', E_USER_DEPRECATED);
+            $this->provider = new LegacyMongoDBCache($collection);
+        } elseif ($collection instanceof Collection) {
+            $this->provider = new ExtMongoDBCache($collection);
+        } else {
+            throw new \InvalidArgumentException('Invalid collection given - expected a MongoCollection or MongoDB\Collection instance');
+        }
     }
 
     /**
@@ -87,19 +87,7 @@ class MongoDBCache extends CacheProvider
      */
     protected function doFetch($id)
     {
-        $document = $this->collection->findOne(['_id' => $id], [self::DATA_FIELD, self::EXPIRATION_FIELD]);
-
-        if ($document === null) {
-            return false;
-        }
-
-        if ($this->isExpired($document)) {
-            $this->createExpirationIndex();
-            $this->doDelete($id);
-            return false;
-        }
-
-        return unserialize($document[self::DATA_FIELD]->bin);
+        return $this->provider->doFetch($id);
     }
 
     /**
@@ -107,19 +95,7 @@ class MongoDBCache extends CacheProvider
      */
     protected function doContains($id)
     {
-        $document = $this->collection->findOne(['_id' => $id], [self::EXPIRATION_FIELD]);
-
-        if ($document === null) {
-            return false;
-        }
-
-        if ($this->isExpired($document)) {
-            $this->createExpirationIndex();
-            $this->doDelete($id);
-            return false;
-        }
-
-        return true;
+        return $this->provider->doContains($id);
     }
 
     /**
@@ -127,20 +103,7 @@ class MongoDBCache extends CacheProvider
      */
     protected function doSave($id, $data, $lifeTime = 0)
     {
-        try {
-            $result = $this->collection->update(
-                ['_id' => $id],
-                ['$set' => [
-                    self::EXPIRATION_FIELD => ($lifeTime > 0 ? new MongoDate(time() + $lifeTime) : null),
-                    self::DATA_FIELD => new MongoBinData(serialize($data), MongoBinData::BYTE_ARRAY),
-                ]],
-                ['upsert' => true, 'multiple' => false]
-            );
-        } catch (MongoCursorException $e) {
-            return false;
-        }
-
-        return isset($result['ok']) ? $result['ok'] == 1 : true;
+        return $this->provider->doSave($id, $data, $lifeTime);
     }
 
     /**
@@ -148,9 +111,7 @@ class MongoDBCache extends CacheProvider
      */
     protected function doDelete($id)
     {
-        $result = $this->collection->remove(['_id' => $id]);
-
-        return isset($result['ok']) ? $result['ok'] == 1 : true;
+        return $this->provider->doDelete($id);
     }
 
     /**
@@ -158,10 +119,7 @@ class MongoDBCache extends CacheProvider
      */
     protected function doFlush()
     {
-        // Use remove() in lieu of drop() to maintain any collection indexes
-        $result = $this->collection->remove();
-
-        return isset($result['ok']) ? $result['ok'] == 1 : true;
+        return $this->provider->doFlush();
     }
 
     /**
@@ -169,46 +127,6 @@ class MongoDBCache extends CacheProvider
      */
     protected function doGetStats()
     {
-        $serverStatus = $this->collection->db->command([
-            'serverStatus' => 1,
-            'locks' => 0,
-            'metrics' => 0,
-            'recordStats' => 0,
-            'repl' => 0,
-        ]);
-
-        $collStats = $this->collection->db->command(['collStats' => 1]);
-
-        return [
-            Cache::STATS_HITS => null,
-            Cache::STATS_MISSES => null,
-            Cache::STATS_UPTIME => (isset($serverStatus['uptime']) ? (int) $serverStatus['uptime'] : null),
-            Cache::STATS_MEMORY_USAGE => (isset($collStats['size']) ? (int) $collStats['size'] : null),
-            Cache::STATS_MEMORY_AVAILABLE  => null,
-        ];
-    }
-
-    /**
-     * Check if the document is expired.
-     *
-     * @param array $document
-     *
-     * @return bool
-     */
-    private function isExpired(array $document) : bool
-    {
-        return isset($document[self::EXPIRATION_FIELD]) &&
-            $document[self::EXPIRATION_FIELD] instanceof MongoDate &&
-            $document[self::EXPIRATION_FIELD]->sec < time();
-    }
-
-    private function createExpirationIndex(): void
-    {
-        if ($this->expirationIndexCreated) {
-            return;
-        }
-
-        $this->expirationIndexCreated = true;
-        $this->collection->createIndex([self::EXPIRATION_FIELD => 1], ['background' => true, 'expireAfterSeconds' => 0]);
+        return $this->provider->doGetStats();
     }
 }
